819441dbcca7d8ab799b27cb5e80725edefe3f1a
Rifa-K-Malik <malikrif07@gmail.com>
2021-10-08 23:24:12 +0530

    Something

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,20 +3,20 @@ import './Checkout.css'
 
 function Checkout() {
     return (
-        <div className="checkout">
-            <div className="checkout--left">
-                <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
-                <div>
-                    <h3 className="checkout--title">Your Shopping Details</h3>
-                    {/* Cart Item here */}
-                    {/* Cart Item here */}
-                </div>
-            </div>
+        // <div className="checkout">
+        //     <div className="checkout--left">
+        //         <img className="checkout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
+        //         <div>
+        //             <h3 className="checkout--title">Your Shopping Details</h3>
+        //             {/* Cart Item here */}
+        //             {/* Cart Item here */}
+        //         </div>
+        //     </div>
 
-            <div className="checkout--left">
-                <h3>SubTotal (2 item):</h3>
-            </div>
-        </div>
+        //     <div className="checkout--left">
+        //         <h3>SubTotal (2 item):</h3>
+        //     </div>
+        // </div>
     )
 }
 
